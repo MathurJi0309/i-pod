f54177e8fc571fe54c2bb8592bbc084ec23892bc
MathurJi <anileshjjssacademy84@gmail.com>
2022-11-20 12:43:54 +0530

    setup the app.js for project

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-
-
-function App(){
-  return(
-    <h1>
-      Hi i ready for project
-    </h1>
-  )
+import React from "react";
+import Wheel from "./Wheel";
+import Screen from "./Screen";
+class App extends React.Component {
+  render() {
+    const { menu, submenu } = this.state;
+    return (
+      <div className="App">
+        <Screen menu={menu} submenu={submenu} />
+        <Wheel
+          onMenuClick={this.handleMenuClick}
+          onhandleRotate={this.handleRotate}
+          handleInnerCirlceClick={this.handleInnerCirlceClick}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
