5f9842a93e61235a9c7c7edeb98adb4be94761f9
MathurJi <anileshjjssacademy84@gmail.com>
2022-11-20 14:33:30 +0530

    create the screen and wheel base

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,23 @@ import React from "react";
 import Wheel from "./Wheel";
 import Screen from "./Screen";
 class App extends React.Component {
+
+    //---- initializing the state
+    constructor() {
+      super();
+  
+      this.state = {
+        menu: false,
+        song: false,
+        games: false,
+        setting: false,
+        developer: false,
+        submenu: false,
+        allsong: false,
+        album: false,
+        artist: false,
+      };
+    }
   render() {
     const { menu, submenu } = this.state;
     return (
